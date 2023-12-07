e7a8966a1313f007ab4f7bf95b2767edcf57aca0
MGissel <144132370+MGissel@users.noreply.github.com>
2023-12-07 10:08:24 +0100

    Update getData.py

--- WIFITest/getData.py
+++ WIFITest/getData.py
@@ -1,15 +1,9 @@
-import serial
-import time
+from serial import Serial
+from time import strftime, localtime
 import csv
 
 # Define the serial port and baud rate
-ser = serial.Serial('COM4', 115200)  # Replace 'COMx' with your ESP32's serial port
-
-wifiIDs = {
-    "6C:31:0E:F3:55:E6": "AAU1",
-    "3C:51:0E:13:FD:86": "AAU2",
-    "6C:31:0E:BA:0A:A6": "AAU3",
-    }
+ser = Serial('COM4', 115200)  # Replace 'COMx' with your ESP32's serial port
 
 def clearData():
     with open('networks.csv', 'w') as file:
@@ -22,7 +16,7 @@ def getData():
             data = ser.readline().decode('utf-8').strip()  # Read a line of data from the serial port
             if data:
                 with open('networks.csv', 'a') as file:
-                    currentTime = str(time.strftime("%H:%M:%S", time.localtime()))
+                    currentTime = str(strftime("%H:%M:%S", localtime()))
                     data = data.split(",")[0:-1]
                     for i in range(int((len(data))/3)):
                         file.write(f"{currentTime},{str(data[(i*3):((i+1)*3)])[1:-1]}\n")
